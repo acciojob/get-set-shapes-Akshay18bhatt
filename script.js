60b2d1da70169509ae8146c73d6ee6660aaca48c
Akshay Bhatt <120771512+Akshay18bhatt@users.noreply.github.com>
2023-12-29 11:17:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ class Rectangle {
 	}
 }
 
-class Square extends Animal {
+class Square extends Rectangle {
 	constructor(side){
 		this.side=side;
 	}
@@ -32,35 +32,6 @@ class Square extends Animal {
 		return (this.side * 4);
 	}
 }
-
-class Animal {
-	constructor(species){
-		this.species=species;
-	}
-	get getSpecies(){
-		return this.species;
-	}
-	makeSound(){
-		console.log(`The ${this.species} makes a sound`);
-	}
-}
-
-class Dog extends Animal {
-	bark(){
-		console.log("woof");
-	}
-}
-
-class Cat extends Animal {
-	purr(){
-		console.log("purr");
-	}
-}
-
-// Do not change the code below this line
-window.Animal = Animal;
-window.Dog = Dog;
-window.Cat = Cat;
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
